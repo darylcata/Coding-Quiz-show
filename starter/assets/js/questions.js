5e87b76d42492928488f7c25d223b8c631208838
Darsing <catalunasiegfried@gmail.com>
2023-01-13 22:02:40 +0000

    added feedback with settimeout

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,12 @@
+var allQuestions = [
+    {
+        question: "Which of these is NOT a Javascript Data type?",
+        choices: ["1. Number", "2. Boolean", "3. Undefined", "4. Function"],
+        answer: 4
+    },
+    {
+        question: "Which of these is NOT a Javascript Data type?",
+        choices: ["1. ", "2. ", "3. ", "4. "],
+        answer: 4
+    }
+];
